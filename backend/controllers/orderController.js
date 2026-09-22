fe718f76564091a7650bc35b7eefc1942c3bd9b6
Aditi Das <replyaditi01@gmail.com>
2026-09-22 22:52:36 +0530

    order testting done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,4 @@
-const Order = require('../models/Order');
+const Order = require('../model/Order');
 const sendEmail = require('../utils/sendEmail');
 
 // Create a new order
@@ -25,6 +25,53 @@ const createOrder = async (req, res) => {
         }
 
     } catch (error) {
-        res.status(400).json({ message: error.message });
+        res.status(500).json({ message: error.message });
     }
+}; 
+
+
+//
+
+const myOrders = async (req,res)=>{
+    try{
+        const orders = await Order.find({user:req.user._id}).populate('items.productId','name price');
+        res.json(orders);
+        
+    }catch(error){
+        res.status(500).json({message:'Error fetching orders',error});
+    }
+};
+
+const getOrders = async (req,res) =>{
+    try{
+    const orders = await Order.find ({}).populate('user','id name')
+     res.json(orders);
+    }catch(error){
+        res.status(500).json({message:'Error fetching orders',error});
+    }
+};
+
+const updateOrderStatus = async(req,res) =>{
+    try{
+        const{status} =req.body;
+        const order = await Order.findById(req.params.id);
+        if(order){
+            order.status = status;
+            await order.save();
+            res.json({message:'order status updated',order});
+        }
+    else{
+        res.status(404).json({message:'order not found'});
+    }
+   }catch(error){
+        res.status(500).json({message:'Error updating orders',error});
+    }
+
+}
+
+module.exports ={
+    createOrder,
+    myOrders,
+    getOrders,
+    updateOrderStatus
 };
